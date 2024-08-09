2ae4b9ef0a1a2c91fd3acece1342cdef6159a386
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2024-08-09 12:12:26 +0900

    (6/8) ✨ Feat : ERC-20 features

--- src/ERC20.sol
+++ src/ERC20.sol
@@ -2,82 +2,93 @@
 
 pragma solidity ^0.8.0;
 
-import "forge-std/interfaces/IERC20.sol";
+import "./IERC20.sol";
+import "./IERC2612.sol";
 
 interface _IERC20 is IERC20 {
     function pause() external;
-
-    function _toTypedDataHash(
-        bytes32 structHash
-    ) external view returns (bytes32);
-
-    function nonces(address owner) external view returns (uint256);
-
-    function permit(
-        address owner,
-        address spender,
-        uint256 value,
-        uint256 deadline,
-        uint8 v,
-        bytes32 r,
-        bytes32 s
-    ) external;
 }
 
-contract ERC20 is _IERC20 {
+contract ERC20 is _IERC20, _IERC2612 {
+    // ===== ERC20 metadata =====
     string public name;
     string public symbol;
+    uint8 public constant decimals = 18;
+
+    // ===== ERC20 state =====
+    mapping(address => uint256) balances;
+    mapping(address => mapping(address => uint256)) allowances;
+    uint public constant totalSupply = 100 ether;
+
+    address private deployer;
+    bool private health = true;
 
     constructor(string memory _name, string memory _symbol) {
+        deployer = msg.sender;
         name = _name;
         symbol = _symbol;
+        balances[deployer] = totalSupply;
     }
 
-    function allowance(
-        address owner,
-        address spender
-    ) external view override returns (uint256) {
-        revert("Not implemented");
-    }
-
-    function approve(
-        address spender,
-        uint256 amount
-    ) external override returns (bool) {
-        revert("Not implemented");
-    }
-
-    function balanceOf(
-        address account
-    ) external view override returns (uint256) {
-        revert("Not implemented");
-    }
-
-    function decimals() external view override returns (uint8) {
-        revert("Not implemented");
-    }
-
-    function totalSupply() external view override returns (uint256) {
-        revert("Not implemented");
+    modifier isHealthy() {
+        require(health);
+        _;
     }
 
     function transfer(
         address to,
         uint256 amount
-    ) external override returns (bool) {
-        revert("Not implemented");
+    ) external override isHealthy returns (bool) {
+        bool res = balances[msg.sender] >= amount;
+        if (res) {
+            balances[msg.sender] -= amount;
+            balances[to] += amount;
+        }
+        return res;
     }
 
     function transferFrom(
         address from,
         address to,
         uint256 amount
+    ) external override isHealthy returns (bool) {
+        bool res = (balances[from] >= amount) &&
+            (allowances[from][msg.sender] >= amount);
+        if (res) {
+            balances[from] -= amount;
+            balances[to] += amount;
+            allowances[from][msg.sender] -= amount;
+        }
+        return res;
+    }
+
+    function approve(
+        address spender,
+        uint256 amount
     ) external override returns (bool) {
-        revert("Not implemented");
+        bool res = (balances[msg.sender] >= amount);
+        if (res) {
+            allowances[msg.sender][spender] += amount;
+        }
+        return res;
+    }
+
+    function allowance(
+        address owner,
+        address spender
+    ) external view override returns (uint256) {
+        return allowances[owner][spender];
+    }
+
+    function balanceOf(
+        address account
+    ) external view override returns (uint256) {
+        return balances[account];
     }
 
     function pause() external override {
-        revert("Not implemented");
+        require(msg.sender == deployer);
+        health = !health;
     }
 
     function _toTypedDataHash(
@@ -98,7 +109,9 @@ contract ERC20 is _IERC20 {
         uint8 v,
         bytes32 r,
         bytes32 s
-    ) external override {
+    ) external override isHealthy {}
+
+    function DOMAIN_SEPARATOR() external view override returns (bytes32) {
         revert("Not implemented");
     }
 }
